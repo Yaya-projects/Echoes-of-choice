8155af8330303b9a6fb14859269929929664e324
Yaya-projects <yahiaihab64@gmail.com>
2026-08-09 16:17:49 +0300

    Update echoes.py

--- echoes.py
+++ echoes.py
@@ -67,16 +67,22 @@ def choices_1 (option_a,):
         break
       else:
        slow_print('You have written an unaccepted value/letter please write a since there are no other options left',speed = 0.02)
-
-
-
+def Haunted_mansion ():
+  slow_print(f'You arrive at the haunted mansion...')
+  slow_print(f'You open the door gently...\nYou enter and a rush of cold wind runs past you.\nYou shiver a bit and look at {dog_name} to see that they are shivering too more than you are.')
+  slow_print(f'The room is barely lit but enough just for you to be able to see just not as well compared to outside.')
+  slow_print(f'As you walk around a bit and look around, you notice that the floor is not just wet but it feels like the floor is shifting with every step you take.\nThe texture of the floor and ceiling look a bit fleshy..')
+  slow_print(f'While your are walking forward {dog_name} stumbles while walking due to the unnatural bending of the floor')
+  
 
 
 def start_game():
      global score
      score = 0
-     slow_print(f'{user_name} wakes up to something licking them.\n you manage to shrug the thing off lightly.\n Once you open your eyes you realise that i was just your dog Buddy trying to wake you up.')
+     global dog_name 
+     slow_print(f'{user_name} wakes up to something licking them.\n you manage to shrug the thing off lightly.\n Once you open your eyes you realise that it was just your dog trying to wake you up.')
+     dog_name = input('Wait what was your dogs name again??').capitalize()
      slow_print(f'You stand up and stretch lightly.\n You look around to find yourself under a tree in vast lush full valley.\n The valley is filled with a variety of flowers and plants!')
-     slow_print(f'While walking along the valley with your beloved dog,You come across an ominous cave infront of you its entrance is a bunch of rocks that are covered with moss and vines...\n,as you look to your left far away theres an ominous haunted mansion it is emitting quite the astral aura...\n and on your right is the village you were brought up in!!\n or you could just hang out with buddy in the valley a bit more!')
-     slow_print('')
-     choices_4(f'Enter the ominous cave.',f'Start heading towards the haunted mansion.',f'Head towards the village.',f'Spend some more time with Buddy in the valley.')
+     slow_print(f'While walking along the valley with your beloved dog,You come across an ominous cave infront of you its entrance is a bunch of rocks that are covered with moss and vines...\n,as you look to your left far away theres an ominous haunted mansion it is emitting quite the astral aura...\n and on your right is the village you were brought up in!!\n or you could just hang out with {dog_name} in the valley a bit more!')
+     slow_print(f'{dog_name} tilts their head and looks towards the cave.\nFor some reason you can sense that {dog_name} is nervous')
+     choices_4(f'Enter the ominous cave.',f'Start heading towards the haunted mansion.',f'Head towards the village.',f'Spend some more time with {dog_name} in the valley.')
